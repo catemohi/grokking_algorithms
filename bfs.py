f9ca67f046b070598870af3b90aa712bd1751174
catemohi <catemohi@gmail.com>
2022-12-20 22:19:45 +0300

    Решение задачи 6.3 из грокаем алгоритмы

--- bfs.py
+++ bfs.py
@@ -10,6 +10,10 @@ from collections import deque
 Из книги Грокаем Алгоритмы задача 6.2
 Найти кратчайший путь от CAB до BAT
 ===============================================================================
+Задача 3
+Из книги Грокаем Алгоритмы задача 6.3
+Проверить списки действий по графу
+===============================================================================
 """
 
 
@@ -36,6 +40,38 @@ def bfs_solution(graph: dict, root_item: str, search_item: str) -> int:
     return None
 
 
+# Нам надо проверить 3 последовательности действий, на действительность.
+# По факту задача сводится к вроверке сосдей в графе
+
+
+def check_actions(graph: dict, actions: list) -> bool:
+    """Проверка можно ли исполнить действия в перечисленном порядке.
+    Args:
+        graph (dict): граф зависимости действий
+        action (list): список действий на проверку
+    Returns:
+        bool: возможена ли такая последовательность
+    """
+    root = actions[0]
+    visited, queue = set(), deque([root])
+    visited.add(root)
+
+    while queue:
+        vertex = queue.popleft()
+        for neighbour in graph[vertex]:
+
+            if neighbour not in visited:
+                visited.add(neighbour)
+                queue.append(neighbour)
+
+        actions.remove(vertex)
+
+        if actions and actions[0] not in queue:
+            return False
+
+    return True
+
+
 if __name__ == "__main__":
     # Задача 1
     # Представим задачу в виде графа
@@ -60,3 +96,23 @@ if __name__ == "__main__":
     assert bfs_solution(graph, 'CAB', 'BAT') == 2, (
         "Задача выполнена не верно! Ваш ответ: "
         f"{bfs_solution(graph, 'CAB', 'BAT')}")
+
+    # Задача 3
+    # Представим граф в виде ассоциативного массива списков
+    graph = {'Проснуться': ['Принять душ', 'Почистить зубы'],
+             'Принять душ': [],
+             'Почистить зубы': ['Позавтракать'],
+             'Позавтракать': [],
+             }
+    actions = ['Проснуться', 'Принять душ', 'Позавтракать', 'Почистить зубы']
+    assert check_actions(graph, actions) is False, (
+        "Задача выполнена не верно! Ваш ответ: "
+        f"{check_actions(graph, actions)}")
+    actions = ['Проснуться', 'Почистить зубы', 'Позавтракать', 'Принять душ']
+    assert check_actions(graph, actions) is True, (
+        "Задача выполнена не верно! Ваш ответ: "
+        f"{check_actions(graph, actions)}")
+    actions = ['Принять душ', 'Проснуться', 'Почистить зубы', 'Позавтракать']
+    assert check_actions(graph, actions) is False, (
+        "Задача выполнена не верно! Ваш ответ: "
+        f"{check_actions(graph, actions)}")
